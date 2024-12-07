d9eeba8a9d88f40c7463c87e21d08a10ffa0f987
Mark Jackson <mark.jackson@red-gate.com>
2024-12-07 12:36:44 +0000

    WIP 3

--- 03/03-b.py
+++ 03/03-b.py
@@ -5,12 +5,12 @@ def get_input_lines(filename):
         lines = file.read()
     return lines
 
-test = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+test_1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
 def get_mul(s):
     # Find all the matches of the pattern in the test string and print them
     # and the two numbers inside the brackets (i.e. the groups)
-    matches = re.findall(r"mul\([0-9]+,[0-9]+\)", s)
+    matches = re.findall(r"mul\([0-9]+,[0-9]+\)|do\(\)", s)
     # matches = re.findall(r"mul", test) # xmul\([0-9]+,[0-9]+\)
     to_return = []
     for match in matches:
@@ -22,7 +22,7 @@ def get_mul(s):
 def sum_of_products(input):
     return sum([x*y for x, y in input])
 
-mul = get_mul(test)
+mul = get_mul(test_1)
 
 assert mul[0] == (2, 4)
 assert mul[1] == (5, 5)
