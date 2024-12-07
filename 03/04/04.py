2e995d241c6bf25965fc31f9e8b370396d325669
Mark Jackson <mark.jackson@red-gate.com>
2024-12-07 14:41:31 +0000

    WIP 4

--- 03/04/04.py
+++ 03/04/04.py
@@ -10,3 +10,20 @@ def get_input_lines(filename):
 
 test_lines = get_input_lines("03/04/test.txt")
 assert len(test_lines) == 10
+
+def xmas_or_dot(lines):
+    to_return = []
+    for l in lines:
+        s = ""
+        for c in l:
+            if c in "XMAS":
+                s += c
+            else: 
+                s += "."
+        to_return.append(s)
+    return to_return
+
+test_ret = xmas_or_dot(["ZXMASZ", "XMAS"])
+print(test_ret)
+assert test_ret[0] == ".XMAS."
+assert test_ret[1] == "XMAS"
