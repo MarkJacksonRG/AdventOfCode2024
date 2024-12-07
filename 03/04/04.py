54bb5ad485eadff71eb0adc182876d9ea4f705d1
Mark Jackson <mark.jackson@red-gate.com>
2024-12-07 15:07:59 +0000

    WIP 4

--- 03/04/04.py
+++ 03/04/04.py
@@ -14,13 +14,11 @@ assert len(test_lines) == 10
 def xmas_or_dot(lines):
     to_return = []
     for l in lines:
-        s = ""
-        for c in l:
-            s += c if c in "XMAS" else "."
+        s = [c if c in "XMAS" else "." for c in l]
         to_return.append(s)
     return to_return
 
 test_ret = xmas_or_dot(["ZXMASZ", "XMAS"])
 print(test_ret)
-assert test_ret[0] == ".XMAS."
-assert test_ret[1] == "XMAS"
+assert test_ret[0] == list(".XMAS.")
+assert test_ret[1] == list("XMAS")
