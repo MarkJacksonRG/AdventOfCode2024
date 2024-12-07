466ab585c887158a61aa622d04226fcf3e2121a4
Mark Jackson <mark.jackson@red-gate.com>
2024-12-07 12:34:48 +0000

    WIP 3

---
+++ 03/03-b.py
@@ -0,0 +1,41 @@
+import re
+
+def get_input_lines(filename):
+    with open(filename, "r") as file:
+        lines = file.read()
+    return lines
+
+test = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+def get_mul(s):
+    # Find all the matches of the pattern in the test string and print them
+    # and the two numbers inside the brackets (i.e. the groups)
+    matches = re.findall(r"mul\([0-9]+,[0-9]+\)", s)
+    # matches = re.findall(r"mul", test) # xmul\([0-9]+,[0-9]+\)
+    to_return = []
+    for match in matches:
+        numbers = re.findall(r"[0-9]+", match)
+        print(match, numbers)
+        to_return.append((int(numbers[0]), int(numbers[1])))
+    return to_return
+
+def sum_of_products(input):
+    return sum([x*y for x, y in input])
+
+mul = get_mul(test)
+
+assert mul[0] == (2, 4)
+assert mul[1] == (5, 5)
+assert mul[2] == (11, 8)
+assert mul[3] == (8, 5)
+
+assert sum_of_products(mul) == 161
+
+lines = get_input_lines("input.txt")
+print (lines)
+real_mul = get_mul(lines)
+print(len(real_mul))
+
+answer = sum_of_products(real_mul)
+print(f"ANSWER IS {answer}")
+
