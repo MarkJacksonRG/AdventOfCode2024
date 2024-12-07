376661a85ed3f9701a01778b9853dbddc9fc7f0f
Mark Jackson <mark.jackson@red-gate.com>
2024-12-07 14:36:55 +0000

    WIP 4

--- 03/04/04.py
+++ 03/04/04.py
@@ -1,2 +1,12 @@
 # test.txt
 # XMAX occurs 18 times
+
+def get_input_lines(filename):
+    with open(filename, "r") as file:
+        lines = file.read()
+
+    # split into separate lines
+    return lines.split("\n")
+
+test_lines = get_input_lines("03/04/test.txt")
+assert len(test_lines) == 10
