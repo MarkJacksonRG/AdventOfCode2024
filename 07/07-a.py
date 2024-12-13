67a1ef0e8fc54cc473d41677151c33c2c2c24813
Mark Jackson <mark.jackson@red-gate.com>
2024-12-13 20:26:43 +0000

    WIP 7

--- 07/07-a.py
+++ 07/07-a.py
@@ -7,3 +7,23 @@ def get_input_lines(filename):
     return lines.split("\n")
 
 test_lines = get_input_lines("test.txt")
+assert len(test_lines) == 9
+
+@dataclass
+class Equation:
+    result: int
+    values: list[int]
+
+def parse_line(line: str) -> Equation:
+    parts = line.split(":")
+    result = int(parts[0])
+    values = [int(x) for x in parts[1].split(" ") if x.isdigit()]
+    return Equation(result, values)
+
+def parse_lines(lines: list[str]) -> list[Equation]:
+    return [parse_line(line) for line in lines]
+
+test_equations = parse_lines(test_lines)
+assert len(test_equations) == 9
+assert test_equations[0] == Equation(190, [10, 19])
+assert test_equations[8] == Equation(292, [11, 6, 16, 20])
