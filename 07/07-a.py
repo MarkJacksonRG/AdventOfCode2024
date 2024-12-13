fcb834a789bd184e5a89400229d8b8588fb81f09
Mark Jackson <mark.jackson@red-gate.com>
2024-12-13 20:21:41 +0000

    WIP 7

---
+++ 07/07-a.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+
+def get_input_lines(filename):
+    with open(filename, "r") as file:
+        lines = file.read()
+    return lines.split("\n")
+
+test_lines = get_input_lines("test.txt")
