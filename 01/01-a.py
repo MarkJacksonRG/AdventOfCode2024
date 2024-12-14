bccdf00961ed2c161f004c617007240ee1d289fe
Mark Jackson <mark.jackson@red-gate.com>
2024-12-14 04:47:06 +0000

    Tidy

--- 01/01-a.py
+++ 01/01-a.py
@@ -11,7 +11,6 @@ def get_input_lines(filename):
 
 
 def get_split_input_lines(filename):
-    global split_lines, line
     lines = get_input_lines(filename)
     split_lines = []
     for line in lines:
@@ -36,32 +35,21 @@ def split_lines_to_two_lists(split_lines):
 def calculate_difference(sorted_pairs):
     sum_diffs = 0
     for pair in sorted_pairs:
-        # print(f"{pair[0]} ---- {pair[1]}")
-        # print(f"{abs(int(pair[0]))} ---- {abs(int(pair[1]))}")
         diff = abs(int(pair[0]) - abs(int(pair[1])))
         sum_diffs += diff
     return sum_diffs
 
-split_lines = get_split_input_lines("input.txt")
-list1, list2 = split_lines_to_two_lists(split_lines)
-# for pair in list(zip(list1, list2)):
-#     print(f"{pair[0]} ---- {pair[1]}")
+real_split_lines = get_split_input_lines("input.txt")
+real_list1, real_list2 = split_lines_to_two_lists(real_split_lines)
 
-print()
-sorted1 = sorted(list1)
-sorted2 = sorted(list2)
-sorted_pairs = list(zip(sorted1, sorted2))
-for pair in sorted_pairs:
-    print(f"{pair[0]} ---- {pair[1]}")
+real_sorted_pairs = list(zip(sorted(real_list1), sorted(real_list2)))
 
 print()
 print("About to calc")
-print (f"DIFFERENCE={calculate_difference(sorted_pairs)}")
+difference = calculate_difference(real_sorted_pairs)
+assert difference == 1660292
+print (f"DIFFERENCE={difference}")
 
-# for line in split_lines:
-#     print(f"{line[0]} ---- {line[1]}")
-#
-# print(len(split_lines))
 
 
 
