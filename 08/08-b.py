fcde119240a5cc1b38461150de64bfc1b01098ed
Mark Jackson <mark.jackson@red-gate.com>
2024-12-14 02:29:38 +0000

    Refactor 8b

--- 08/08-b.py
+++ 08/08-b.py
@@ -1,9 +1,7 @@
 from dataclasses import dataclass
-
 import numpy as np
 
-
-def get_input_lines(filename):
+def get_input_lines(filename: str) -> list[str]:
     with open(filename, "r") as file:
         lines = file.read()
     return lines.split("\n")
@@ -12,21 +10,20 @@ test_lines = get_input_lines("test.txt")
 assert len(test_lines) == 12
 
 class Board:
-    def __init__(self, lines):
+    def __init__(self, lines: list[str]):
         self.lines = [list(l) for l in lines]
         assert all(len(row) == len(self.lines[0]) for row in self.lines)
 
-    def get(self, x, y):
-        # x means left/right (positive is right) and y means up/down (positive is down)
+    def get(self, x: int, y: int) -> str:
         if x not in self.get_x_range() or y not in self.get_y_range():
             return "-"
         else:
             return self.lines[y][x]
 
-    def get_x_range(self):
+    def get_x_range(self) -> range:
         return range(len(self.lines[0]))
 
-    def get_y_range(self):
+    def get_y_range(self) -> range:
         return range(len(self.lines))
 
 test_board = Board(test_lines)
@@ -36,10 +33,10 @@ assert test_board.get(-1, 6) == "-"
 assert test_board.get(6, 5) == "A"
 assert test_board.get(4, -1) == "-"
 
-def make_point_nparray(x: int, y:int):
+def make_point_nparray(x: int, y: int) -> np.ndarray:
     return np.array([x, y])
 
-def get_frequencies(board: Board):
+def get_frequencies(board: Board) -> dict[str, list[np.ndarray]]:
     frequencies = {}
     for y in board.get_y_range():
         for x in board.get_x_range():
@@ -59,13 +56,14 @@ assert len(test_frequencies["A"]) == 3
 assert (test_frequencies["0"][0] == np.array((8, 1))).all()
 assert (test_frequencies["0"][3] == np.array((4, 4))).all()
 
-def get_pairs(board: Board, frequency: str):
+def get_pairs(board: Board, frequency: str) -> list[tuple[np.ndarray, np.ndarray]]:
     pairs = []
     points = get_frequencies(board).get(frequency)
     for i in range(len(points)):
         for j in range(i + 1, len(points)):
             pairs.append((points[i], points[j]))
     return pairs
+
 test_pairs1 = get_pairs(test_board, "0")
 assert len(test_pairs1) == 6
 assert (make_point_nparray(8, 1) == test_pairs1[2][0]).all()
