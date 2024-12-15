b3df529134afc5a74653a031a860e9f8afd689de
Mark Jackson <mark.jackson@red-gate.com>
2024-12-15 06:35:49 +0000

    WIP 10a

---
+++ 10/10-a.py
@@ -0,0 +1,30 @@
+def get_input_lines(filename):
+    with open(filename, "r") as file:
+        lines = file.read()
+    return lines.split("\n")
+
+test_lines = get_input_lines("test.txt")
+assert len(test_lines) == 8
+
+class Board:
+    def __init__(self, lines):
+        self.lines = [list(l) for l in lines]
+        assert all(len(row) == len(self.lines[0]) for row in self.lines)
+
+    def get(self, x, y):
+        # x means left/right (positive is right) and y means up/down (positive is down)
+        if x not in self.get_x_range() or y not in self.get_y_range():
+            return "-"
+        else:
+            return self.lines[y][x]
+
+    def get_x_range(self):
+        return range(len(self.lines[0]))
+
+    def get_y_range(self):
+        return range(len(self.lines))
+
+test_board = Board(test_lines)
+
+assert test_board.get(4, 2) == "0"
+
