062b7a8730c77518c7ad4bef1c5a918f23e4abf6
Mark Jackson <mark.jackson@red-gate.com>
2024-12-15 06:43:19 +0000

    WIP 10a

--- 10/10-a.py
+++ 10/10-a.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def get_input_lines(filename):
     with open(filename, "r") as file:
         lines = file.read()
@@ -28,3 +31,12 @@ test_board = Board(test_lines)
 
 assert test_board.get(4, 2) == "0"
 
+def get_scores(board: Board):
+    # Create numpy array of integers with the same dimensions as the board
+    # and initialize it with -1
+    scores = np.array([[-1 for _ in board.get_x_range()] for _ in board.get_y_range()])
+    assert scores[0][0] == -1
+    assert scores[board.get_x_range().stop-1][board.get_y_range().stop-1] == -1
+    return scores
+
+get_scores(test_board)
