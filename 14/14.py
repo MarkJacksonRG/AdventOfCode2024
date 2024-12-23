04bcf908a6a6f3ef2a6f177b26e0fc34ea6fcbb2
Mark Jackson <mark.jackson@red-gate.com>
2024-12-23 13:37:13 +0000

    WIP 14

--- 14/14.py
+++ 14/14.py
@@ -8,10 +8,10 @@ from helpers.helpers import get_input_lines
 
 @dataclass(frozen=True)
 class Robot:
-    p: np.array
-    v: np.array
+    p: np.ndarray
+    v: np.ndarray
 
-def to_2d_array(s: str) -> np.array:
+def to_2d_array(s: str) -> np.ndarray:
     assert s[1] == "="
     return np.array([int(x) for x in s[2:].split(",")])
 
@@ -24,11 +24,25 @@ def get_robots_from_lines(lines: list[str]) -> list[Robot]:
         robots.append(Robot(pos, direction))
     return robots
 
-def count_robots_on_position(robots: list[Robot], p: np.array) -> int:
-    return sum([1 for r in robots if (r.p == p).all()])
+def count_robots_on_position(positions: list[np.ndarray], p: np.ndarray) -> int:
+    return sum([1 for next_p in positions if (next_p == p).all()])
 
-def get_p_after_t(r: Robot, t: int, space: np.array) -> np.array:
+def get_p_after_t(r: Robot, t: int, space: np.ndarray) -> np.ndarray:
     return (r.p + r.v * t) % space
+
+def get_safety_factor(positions: list[np.ndarray], space: np.ndarray) -> int:
+    tl, tr, bl, br = 0, 0, 0, 0
+    mid_x, mid_y = int(space[0] / 2), int(space[1] / 2)
+    for p in positions:
+        if p[0] < mid_x and p[1] < mid_y:
+            tl += 1
+        elif p[0] > mid_x and p[1] < mid_y:
+            tr += 1
+        elif p[0] < mid_x and p[1] > mid_y:
+            bl += 1
+        elif p[0] > mid_x and p[1] > mid_y:
+            br += 1
+    return tl * tr * bl * br
 
 @pytest.fixture
 def test_input():
@@ -47,13 +61,14 @@ def test_get_robots_from_lines(test_input):
 
 def test_count_robots_on_position(test_input):
     robots = get_robots_from_lines(test_input)
+    positions = [r.p for r in robots]
 
-    assert count_robots_on_position(robots, np.array([0, 4])) == 1
-    assert count_robots_on_position(robots, np.array([2, 4])) == 1
-    assert count_robots_on_position(robots, np.array([0, 0])) == 1
-    assert count_robots_on_position(robots, np.array([1, 0])) == 0
-    assert count_robots_on_position(robots, np.array([2, 0])) == 1
-    assert count_robots_on_position(robots, np.array([3, 0])) == 2
+    assert count_robots_on_position(positions, np.array([0, 4])) == 1
+    assert count_robots_on_position(positions, np.array([2, 4])) == 1
+    assert count_robots_on_position(positions, np.array([0, 0])) == 1
+    assert count_robots_on_position(positions, np.array([1, 0])) == 0
+    assert count_robots_on_position(positions, np.array([2, 0])) == 1
+    assert count_robots_on_position(positions, np.array([3, 0])) == 2
 
 
 def test_get_p_after_t():
@@ -71,7 +86,16 @@ def test_get_p_after_t_hundred_seconds(test_input):
 
     p100 = [get_p_after_t(r, 100, space) for r in robots]
 
-    assert (get_p_after_t(r, 4, space) == np.array([10, 6])).all()
-    assert (get_p_after_t(r, 5, space) == np.array([1, 3])).all()
-    assert (get_p_after_t(r, 6, space) == np.array([3, 0])).all()
-    assert (get_p_after_t(r, 7, space) == np.array([5, 4])).all()
+    assert count_robots_on_position(p100, np.array([0, 0])) == 0
+    assert count_robots_on_position(p100, np.array([6, 0])) == 2
+    assert count_robots_on_position(p100, np.array([9, 0])) == 1
+    assert count_robots_on_position(p100, np.array([0, 2])) == 1
+
+
+def test_get_safety_factor_after_t_hundred_seconds(test_input):
+    space = np.array([11, 7])
+    robots = get_robots_from_lines(test_input)
+
+    p100 = [get_p_after_t(r, 100, space) for r in robots]
+
+    assert get_safety_factor(p100, space) == 12
