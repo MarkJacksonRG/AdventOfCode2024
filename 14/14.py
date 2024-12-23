61bb65fef2c3a40e2d2c0ffc4ef2bc3155afc220
Mark Jackson <mark.jackson@red-gate.com>
2024-12-23 13:19:50 +0000

    WIP 14

--- 14/14.py
+++ 14/14.py
@@ -24,6 +24,9 @@ def get_robots_from_lines(lines: list[str]) -> list[Robot]:
         robots.append(Robot(pos, direction))
     return robots
 
+def count_robots_on_position(robots: list[Robot], p: np.array) -> int:
+    return sum([1 for r in robots if (r.p == p).all()])
+
 def get_p_after_t(r: Robot, t: int, space: np.array) -> np.array:
     return (r.p + r.v * t) % space
 
@@ -42,6 +45,17 @@ def test_get_robots_from_lines(test_input):
     assert (robots[1].p == np.array([6, 3])).all()
     assert (robots[1].v == np.array([-1, -3])).all()
 
+def test_count_robots_on_position(test_input):
+    robots = get_robots_from_lines(test_input)
+
+    assert count_robots_on_position(robots, np.array([0, 4])) == 1
+    assert count_robots_on_position(robots, np.array([2, 4])) == 1
+    assert count_robots_on_position(robots, np.array([0, 0])) == 1
+    assert count_robots_on_position(robots, np.array([1, 0])) == 0
+    assert count_robots_on_position(robots, np.array([2, 0])) == 1
+    assert count_robots_on_position(robots, np.array([3, 0])) == 2
+
+
 def test_get_p_after_t():
     space = np.array([11, 7])
     r = Robot(np.array([2, 4]), np.array([2, -3]))
@@ -50,3 +64,14 @@ def test_get_p_after_t():
     assert (get_p_after_t(r, 1, space) == np.array([4, 1])).all()
     assert (get_p_after_t(r, 2, space) == np.array([6, 5])).all()
     assert (get_p_after_t(r, 3, space) == np.array([8, 2])).all()
+
+def test_get_p_after_t_hundred_seconds(test_input):
+    space = np.array([11, 7])
+    robots = get_robots_from_lines(test_input)
+
+    p100 = [get_p_after_t(r, 100, space) for r in robots]
+
+    assert (get_p_after_t(r, 4, space) == np.array([10, 6])).all()
+    assert (get_p_after_t(r, 5, space) == np.array([1, 3])).all()
+    assert (get_p_after_t(r, 6, space) == np.array([3, 0])).all()
+    assert (get_p_after_t(r, 7, space) == np.array([5, 4])).all()
